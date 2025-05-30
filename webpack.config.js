9eae2950c353b60b5994059cbbc62938b42e39cf
Alexandre-dotDev <alexandre.centro@yahoo.com.br>
2025-05-30 20:32:46 -0300

    Add webpack.config e plugins mais babel

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  mode: "development",
+  devServer: {
+    static: "./dist",
+    hot: true,
+    port: 3000,
+    open: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: "/node_modules",
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "App-Numbers",
+      template: "./src/index.html",
+      favicon: "./src/assets/icon-1.svg",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./src/assets", to: "assets" }],
+    }),
+  ],
+};
