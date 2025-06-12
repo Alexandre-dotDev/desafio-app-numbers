24dfa5df7a167f9c893736c2772e2fddce372d3f
Alexandre-dotDev <alexandre.centro@yahoo.com.br>
2025-06-12 00:41:10 -0300

    Modified em diversos arquivos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import "./css/index.css";
+import "./css/layout.css";
+import { drawerCalc } from "./js/drawer";
+import { resetStorage } from "./js/utils/reset-storage";
+const fieldsetResults = document.querySelector(".resultsID");
+
+document.addEventListener("DOMContentLoaded", () => {
+  fieldsetResults.classList.add("disabled");
+  resetStorage();
+  drawerCalc();
+});
